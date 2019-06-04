9a13ed6ab9294d130ae161b938b13d0389d50647
Suhas Vijaykumar <suhas.vk@gmail.com>
2019-06-04 10:49:20 -0400

    coded value function iteration procedure

--- dgp.py
+++ dgp.py
@@ -6,11 +6,13 @@
 # estimators
 
 import numpy as np 
-import random
+import matplotlib.pyplot as mpl
 
+from scipy.interpolate import interp1d
+from scipy.stats import norm as normal
 # Set the random seed for replicability
 RANDOM_SEED = 13
-numpy.random.seed(RANDOM_SEED)
+np.random.seed(RANDOM_SEED)
 
 # Set the sample size
 n = 1000
@@ -18,31 +20,83 @@ n = 1000
 # Set the dimension of the (high-dimensional, static) component of the state
 p = 1000
 
-def compute_value_function(grid, Theta, Unobservables):
+# Set the size of support of exactly sparse vectors
+s = 10
 
-	pass
+# Set the discount parameter
+beta = 0.75
 
-# This function will generate an approximately sparse signal of the form Theta_s + Theta_d
+def compute_value_function(Theta, Unobservables, grid=np.linspace(-100,100,10e4), precision=1e-6, draws=10e5):
+
+	# Get value function parameters
+	A0, A1, B0, B1 = Unobservables
+
+	# These are the action specific AR coefficients
+	AR0 = np.dot(A0,Theta[1:])
+	AR1 = np.dot(A1,Theta[1:])
+
+	# These are the action specific utility constants
+	C0 = np.dot(B0[1:], Theta[1:])
+	C1 = np.dot(B1[1:], Theta[1:])
+
+	# These are the action specific utility coefficients on the dynamic state
+	D0 = B0[0]
+	D1 = B1[0]
+
+	# Compute discrete approximation to normal distribution
+	jump_density = normal.pdf(grid)
+
+	# Compute the initial value function values
+	V_old = np.zeros(grid.shape)
+	V_new = np.maximum(
+		C0 + np.multiply(D0,grid), 
+		C1 + np.multiply(D1,grid))
+
+	while np.amax(np.abs(V_old-V_new)) > precision:
+
+		V_old = V_new
+
+		# The function "f" maps x to EV(x + e) where e ~ N(0,1) 
+		f = interp1d(grid, np.convolve(V_old,jump_density))
+
+		# The continuation values contain EV(Ax + e) for each value in the grid
+		CV0 = f(np.multiply(AR0,grid))
+		CV1 = f(np.multiply(AR1,grid))
+
+		# Given all this, we can compute the next step of value function iteration
+		V_new = np.maximum(
+		C0 + np.multiply(D0,grid) + beta*CV0, 
+		C1 + np.multiply(D1,grid) + beta*CV1)
+
+	# VF contains our final estimate of the value function
+	VF = V_new
+
+	# Policy contains the optimal action for each value in the grid
+	Policy = C0 + np.multiply(D0,grid) + beta*CV0 < np.multiply(D1,grid) + beta*CV1
+	
+
+
+# This function will generate an approximately sparse signal of the form Theta_1 + Theta_2
 # - p = dimension of vector
-# - s = size of support of Theta_s
-# - Theta_d has k'th coordinate U_k/k^2 where U_k is Unif[-1,1].
-def approximately_sparse(p, s=10, true_sparse_component=True, decay_component=True):
+# - s = size of support of (truly sparse) Theta_1
+# - Theta_2 (apx. sparse) has k'th coordinate U_k/k^2 where U_k is Unif[-1,1].
+def approximately_sparse(p, s=s, true_sparse_component=True, decay_component=True):
  	
 	dense_part = np.multiply(
 		1 - 2 * np.random.random_sample(size=(p,)),
-		np.array(map(lambda x: float(1)/(x**2), range(1,p+1)))
+		1./np.power(np.array(range(1,p+1)),2)
 	)
 
 	sparse_support = np.random.choice(p,s)
 	sparse_coefficients = 1 - 2 * np.random.random_sample(size=(s,))
 
 	sparse_part = np.zeros(p)
- 	sparse_part[sparse_support] = sparse_coefficients
+	sparse_part[sparse_support] = sparse_coefficients
 
- 	return dense_part + sparse_par
+	return dense_part*int(decay_component) + sparse_part*int(true_sparse_component)
 
 # This generates the (observable) state of the agent
-def generate_observable_parameters(n,p):
+def generate_observable_parameters(n=n,p=p):
 
 	# This is the dynamic state (drawn from a standard normal distribution)
 	ThetaD = np.random.randn(1)
@@ -50,20 +104,32 @@ def generate_observable_parameters(n,p):
 	# This is the static state (high dimensional, binary/categorical)
 	ThetaS = np.random.randint(2,size=(p,))
 
-	return np.hstack((Theta0,ThetaS))
+	return np.hstack((ThetaD,ThetaS))
 
 # This generates the (unobserbable) utility function and state evolution parameters
-def generate_unobservable_parameters(n,p,sigma_epsilon_utility, sigma_epsilon_state):
+def generate_unobservable_parameters(n=n,p=p):
 
 	# These determine the state-specific utility function
 	B0 = approximately_sparse(p+1)
 	B1 = approximately_sparse(p+1)
 
-	# These are the coefficients that determine the AR coefficients for the dynamic state
-	A0 = approximately_sparse(p)
-	A1 = approximately_sparse(p)
+	# These are the multipliers that determine  AR coefficients for the dynamic state
+	# We divide by s+1 to ensure that AR coefficients lie strictly between zero and one
+	A0 = np.absolute(approximately_sparse(p, decay_component=False)) / (s+1) + 1./np.power(p,2)
+	A1 = np.absolute(approximately_sparse(p, decay_component=False)) / (s+1) + 1./np.power(p,2)
 
 	return (A0,A1,B0,B1)
+
+def test():
+	Theta = generate_observable_parameters()
+	Unobservables = generate_unobservable_parameters()
+
+	VF0 = compute_value_function(Theta, Unobservables)
+
+
+
+if __name__ == '__main__':
+	test()
 
 
 	
