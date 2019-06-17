1f5e03a19d6aea5de94fa6b4a623fec71d8a6de1
Suhas Vijaykumar <suhasv@supply.mit.edu>
2019-06-16 20:46:32 -0400

    finished policy computation

--- compute_policy.py
+++ compute_policy.py
@@ -1,54 +1,103 @@
 # compute_policy.py
+# encoding: utf-8
 # Suhas Vijaykumar, June 2019
 # This file contains the value function iteration procedure.
 # Requires debugging.
 
 import numpy as np
-from scipy.interpolate import interpn, interp1d
-from numba import prange
+from numba import prange, njit
+
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as mpl
 
 def compute_policy_dummy(model_instance, grid=np.linspace(-200,200,10e4)):
 	return {"value_function": np.array([0 for _ in grid]), "policy": grid > 0, "grid": grid}
 	
-def compute_policy_q_learning(model_instance):
-	pass
-	
-def compute_policy_vf_iteration(model_agent, X=np.linspace(-200,200,10e4), precision=1e-6, ε_vector=np.random.randn(10e5)):
-	
-	expected_utility = np.vectorize(lambda a, x : model_agent.utility(dynamic_state=x, action=x, shock=0))
-	transition_kernel = lambda a, x, ε : model_agent.next_state(dynamic_state=x, action=a, shock=ε)
-	
-	
-	β = model_agent.discount_factor
-	
-	
-	δ = lambda v1, v2: np.amax(np.abs(v1-v2))
-	
-	def bellman_operator(Vf):
-		TV = np.empty_like(grid)
-		for i in prange(grid.shape[0]):
-			x = grid[i,:]
-			t1 = lambda ε : transition(1, x, ε)
-			t0 = lambda ε : transition(0, x, ε)
-			TV[i,:] = max(
-				EU(1,x) + β * np.mean(V(t1(ε_vector))),
-				EU(0,x) + β * np.mean(V(t0(ε_vector)))
-			) 
-		return TV	
+def q_learning(model_instance):
+	pass	
 
-	# Compute the initial value function values
-	V_old = np.empty_like(grid)
-	V_new = bellman_operator(lambda x: 0)
+# This computes an approximation to the Bellman operator obtained by substituting the 
+# normal distribution with the empirical distribution of 10e3 draws from the normal 
+# distribution (by Monte Carlo integration) 
+@njit
+def bellman_operator(V, X, TX0, TX1, EU0, EU1, random_draws, b):
+	TV = np.empty_like(V)
+	Policy = np.empty_like(V)
+	for i in prange(X.shape[0]):
+		x = X[i]
 
-	while δ(V_new,V_old) > precision:
-		V_old = V_new
-		Vf = interp1d(grid,V_old,assume_sorted=True)
-		V_new = bellman_operator
-
-	# VF contains our final estimate of the value function
-	VF = V_new
-
-	# Policy contains the optimal action for each value in the grid
-	Policy = C0 + np.multiply(D0,grid) + beta*CV0 < np.multiply(D1,grid) + beta*CV1
+		V1 = np.mean( np.interp(TX1[i] + random_draws, X, V) )
+		V0 = np.mean( np.interp(TX0[i] + random_draws, X, V) )
 	
-	return {"value_function": VF, "policy": policy, "grid": grid}
+		TV[i] = np.maximum(
+			EU1[i] + b * V1,
+			EU0[i] + b * V0
+		) 
+		Policy[i] = np.greater_equal(
+			EU1[i] + b * V1,  
+			EU0[i] + b * V0
+		)
+	return TV, Policy
+
+@njit 
+def vf_iteration(X, TX0, TX1, EU0, EU1, b, n_draws, precision, verbose):
+
+	print("computing value function by vf_iteration")
+	V0 = np.zeros(X.shape[0])
+	V1 = np.fmax(EU0, EU1)
+	random_draws = np.random.randn(n_draws)
+
+	# The uniform distance between two piecewise-linear approximate value functions 
+	distance = lambda v1, v2: np.amax(np.abs(v1-v2))
+
+	iterations = 0
+	while distance(V1,V0) > precision:
+		V0 = V1
+		V1, Policy = bellman_operator(V0, X, TX0, TX1, EU0, EU1, random_draws, b)
+		iterations += 1
+		if verbose:
+			print("iterations:", iterations)
+			print("distance:", distance(V1,V0))
+
+	return V1, Policy
+
+def compute_policy(model_agent, grid=np.linspace(-200,200,10e4), precision=1e-6, n_draws = 1000, verbose=False, method = 'vf_iteration'):
+	
+	expected_utility = np.vectorize(lambda a, x : model_agent.utility(action=a, shock=0, dynamic_state=x))
+	transition_kernel = np.vectorize(lambda a, x : model_agent.next_state(action=a, shock=0, dynamic_state=x, update=False))
+
+	# The agent's discount factor is given by b	
+	b = model_agent.model.discount_factor
+
+	# TX0 = transition_kernel(grid,0)
+	# print(TX0.shape)
+	# print(TX0[10])
+
+	if method is 'vf_iteration':
+		ValueFunction, OptimalPolicy = vf_iteration(
+			X = grid,
+			TX0 = transition_kernel(grid,0),
+			TX1 = transition_kernel(grid,1),
+			EU0 = expected_utility(grid,0),
+			EU1 = expected_utility(grid,1),
+			b = b,
+			n_draws = n_draws,
+			precision = precision,
+			verbose = verbose
+		) 
+
+	else:
+		raise Exception("Dynamic programming method %s not supported!" % method)
+
+	return (ValueFunction, OptimalPolicy)
+
+	
+def test_vf_iteration():
+	model = BasicModel()
+	agent = BasicModelAgent(model)
+	policy, V_final = compute_policy(agent, verbose=True, method='vf_iteration')
+	
+if __name__ == '__main__':
+	from dgp import *
+	test_vf_iteration()	
